9c29663fed651486e8010be20a925aa5936683d9
Istian Johnson <130726890+IstianJohnson@users.noreply.github.com>
2024-09-02 18:04:26 +0530

    Prime Or Not.py
    This Program Checks That Wether the Entered number is prime or not and Also give not Prime When the Number Is Less then or equal to 1

---
+++ Prime Or Not.py
@@ -0,0 +1,11 @@
+a = int(input("Enter The Number : "))
+if a <= 1:
+    print("It is not Prime Number")
+else:
+    for i in range (2,a):
+        if a % i == 0:
+            k = "Not Prime"
+            break
+        else:
+            k = "Prime"
+    print("The Entered Number is",k)
